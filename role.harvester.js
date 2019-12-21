19b25be05d3056d9e27ad3bd5f115549e47a2638
Joel Triemstra <>
2019-12-21 17:48:26 -0500

    harvest for the tower as well

--- role.harvester.js
+++ role.harvester.js
@@ -11,8 +11,10 @@ var roleHarvester = {
         else {
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                        return (structure.structureType == STRUCTURE_EXTENSION ||
+                                structure.structureType == STRUCTURE_SPAWN ||
+                                structure.structureType == STRUCTURE_TOWER) && 
+                                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                     }
             });
             if(targets.length > 0) {
